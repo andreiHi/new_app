a1a065a18181448723700274c4723ca5c917fcc9
Hincu Andrei <andreih1981@gmail.com>
2019-04-21 19:53:33 +0300

    spring boot + Angular

--- front/src/main/web/src/app/search/search.component.ts
+++ front/src/main/web/src/app/search/search.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+    topicList: string[];
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
-
+    getTopicList(){
+        this.topicList =  ['topic1', 'topic2', 'topic3'];
+    }
 }
