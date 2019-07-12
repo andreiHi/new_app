04fe29b316c020f62c12b260879e0cccf6ad82cd
Hincu Andrei <andreih1981@gmail.com>
2019-07-12 14:04:28 +0300

    add Proc file

--- back/src/main/java/com/app/controller/MainController.java
+++ back/src/main/java/com/app/controller/MainController.java
@@ -1,11 +1,7 @@
 package com.app.controller;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
